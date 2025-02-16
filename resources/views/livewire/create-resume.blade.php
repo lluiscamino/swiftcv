9960f5c3f179a89ea6791c15885e54d490a3f55b
Lluís Camino <caminoperez.lluis@gmail.com>
2025-02-16 19:40:42 +0000

    Fix form submit button style on mobile
    Fixes #30

--- resources/views/livewire/create-resume.blade.php
+++ resources/views/livewire/create-resume.blade.php
@@ -23,7 +23,7 @@
             <x-forms.resume-section :resumeSectionType="$sectionType" :form="$form"/>
         @endforeach
         <button type="submit"
-                class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800">
+                class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800 w-full sm:w-auto mt-4">
             Create my resume
         </button>
     </form>
