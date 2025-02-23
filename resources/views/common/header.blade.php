8bc46fa077044492edcf03af689e8c833a8a0897
Lluís Camino <caminoperez.lluis@gmail.com>
2025-02-23 18:01:19 +0000

    Update tags inside <head>, and add web manifest
    Fixes #24

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -1,5 +1,7 @@
-@php($pageTitle = config('app.name') . ' - ' . config('app.description'))
-        <!DOCTYPE html>
+@php($pageDescription = config('app.description'))
+@php($pageTitle = config('app.name') . ' - ' . $pageDescription)
+@php($screenshot = url('screenshot.png'))
+<!DOCTYPE html>
 <html lang="{{ config('app.locale')  }}">
 <head>
     <meta charset="UTF-8">
@@ -9,25 +11,21 @@
 
     <!-- Meta SEO -->
     <meta name="title" content="{{ $pageTitle }}">
-    <meta name="description"
-          content="Get started with a free and open-source landing page built with Tailwind CSS and the Flowbite component library.">
+    <meta name="description" content="{{ $pageDescription }}">
     <meta name="robots" content="index, follow">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="language" content="English">
-    <meta name="author" content="Themesberg">
 
     <!-- Social media share -->
     <meta property="og:title" content="{{ $pageTitle  }}">
-    <meta property="og:site_name" content="Themesberg">
-    <meta property="og:url" content="https://https://demo.themesberg.com/landwind/">
-    <meta property="og:description"
-          content="Get started with a free and open-source landing page for Tailwind CSS built with the Flowbite component library featuring dark mode, hero sections, pricing cards, and more.">
-    <meta property="og:type" content="">
-    <meta property="og:image"
-          content="https://themesberg.s3.us-east-2.amazonaws.com/public/github/landwind/og-image.png">
-    <meta name="twitter:card" content="summary"/>
-    <meta name="twitter:site" content="@themesberg"/>
-    <meta name="twitter:creator" content="@themesberg"/>
+    <meta property="og:site_name" content="{{ $pageTitle }}">
+    <meta property="og:url" content="{{ url()->current() }}">
+    <meta property="og:description" content="{{ $pageDescription }}">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="{{ $screenshot }}">
+    <meta name="twitter:card" content="{{ $screenshot }}"/>
+    <meta name="twitter:title" content="{{ $pageTitle }}"/>
+    <meta name="twitter:description" content="{{ $pageDescription }}"/>
 
     <!-- Favicon -->
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
@@ -35,8 +33,8 @@
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <link rel="manifest" href="/site.webmanifest">
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet"/>
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
+    <meta name="msapplication-TileColor" content="#046c4e">
+    <meta name="theme-color" content="#046c4e">
     @vite('resources/css/app.css')
     @livewireStyles
 </head>
