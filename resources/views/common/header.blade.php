bb77485a69bd93e992e45dec8fb6a002a052a41c
Lluís Camino <caminoperez.lluis@gmail.com>
2025-02-23 15:13:04 +0000

    Fix swiper images overlapping with header

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -41,7 +41,7 @@
     @livewireStyles
 </head>
 <body>
-<header class="fixed w-full">
+<header class="fixed w-full z-10">
     <nav class="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
         <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
             <a href="/" class="flex items-center">
