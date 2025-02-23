1043c2cc126bf5710d1d8710911a0d6109b5a644
Lluís Camino <caminoperez.lluis@gmail.com>
2025-02-23 16:31:18 +0000

    Sync form values on blur, instead of on every change

--- resources/views/components/forms/textarea.blade.php
+++ resources/views/components/forms/textarea.blade.php
@@ -4,7 +4,7 @@
     </label>
     <textarea
             class="appearance-none block w-full bg-gray-50 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-            wire:model.live="form.{{ $id }}"
+            wire:model.blur="form.{{ $id }}"
             type="{{ $type }}"
             placeholder="{{ $placeholder ?? '' }}"
             autoComplete="off"
