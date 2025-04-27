66ef102fb42d8d225961a8ce49549b7ea9d14cca
Lluís Camino <caminoperez.lluis@gmail.com>
2025-04-27 21:31:45 +0100

    Add Google Analytics tag

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -4,6 +4,15 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale')  }}">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-705SRZQ6JY"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-705SRZQ6JY');
+    </script>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
