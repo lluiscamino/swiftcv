87db918856e6474f0f1b4b6f03b4845149ace6fe
Lluís Camino <caminoperez.lluis@gmail.com>
2025-04-27 19:27:13 +0100

    Update footer
    Fixes #7

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,5 +1,6 @@
 <footer class="bg-white dark:bg-gray-800">
     <div class="max-w-screen-xl p-4 py-6 mx-auto lg:py-16 md:p-8 lg:p-10">
+<?php /*
         <div class="grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-5">
             <div>
                 <h3 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Company</h3>
@@ -85,6 +86,7 @@
             </div>
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8">
+*/ ?>
         <div class="text-center">
             <a href="#"
                class="flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
@@ -92,7 +94,7 @@
                 {{ config('app.name') }}
             </a>
             <span class="block text-sm text-center text-gray-500 dark:text-gray-400">© {{ date('Y') }} {{ config('app.name') }}. All Rights Reserved.</span>
-            <!--
+<?php /*
             <ul class="flex justify-center mt-5 space-x-5">
                 <li>
                     <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
@@ -138,7 +140,7 @@
                     </a>
                 </li>
             </ul>
-            -->
+*/ ?>
         </div>
     </div>
 </footer>
