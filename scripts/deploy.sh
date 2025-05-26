f9b63a72614813635e3e966f5086fae7fe72c363
Lluís Camino <caminoperez.lluis@gmail.com>
2025-05-26 14:27:57 +0100

    Update EB env name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@ COMMIT_TAG=$(git rev-parse HEAD)
 
 cd ..
 git checkout "$COMMIT_TAG"
-eb use SwiftCV-env
-eb deploy SwiftCV-env --label="$COMMIT_TAG"
+eb use SwiftCV-env-1
+eb deploy SwiftCV-env-1 --label="$COMMIT_TAG"
 git checkout main
